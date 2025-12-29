50eb5426099df0b11d6a12e2906f0c731cc4075f
LevinKrischer <levin@levinkrischer.de>
2025-12-29 12:51:54 +0100

    fix: change sizes of characters

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,7 +1,8 @@
 class Chicken extends MoveableObject {
 
-    height = 100 
-    y = 330;
+    height = 60;
+    width = 60; 
+    y = 365;
     constructor() {
         super().loadImage("assets/img/3_enemies_chicken/chicken_normal/1_walk/1_w.png");
 
